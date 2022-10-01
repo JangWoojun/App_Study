6cf1c5d0c89d1629ca66dbfdb9efb314bbe0e249
JangWoojun <woojun0107@naver.com>
2022-10-01 22:49:16 +0900

    버튼 클릭 수 세는 앱 완성

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.hello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvMyTextView = findViewById<TextView>(R.id.textView)
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        var timesClick = 0
+
+        btnClickMe.setOnClickListener {
+            timesClick+=1
+
+            tvMyTextView.text = timesClick.toString()
+            Toast.makeText(this,timesClick.toString()+"번 클릭하셨습니다",Toast.LENGTH_LONG).show()
+        }
+
+
+
     }
 }
